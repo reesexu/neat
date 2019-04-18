49062a5b0e5be2853ff6f3fc183b4d799f835066
xuwenchao <842390367@qq.com>
2019-04-19 00:27:01 +0800

    add debounce&throttle&pullDownRefresh

--- miniprogram/constants/index.js
+++ miniprogram/constants/index.js
@@ -1,2 +1,2 @@
 // 标题最大长度
-export const TITLE_MAX_LENGTH = 120
+export const TITLE_MAX_LENGTH = 12
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,10 @@
-import {regeneratorRuntime} from '../../lib/index'
-import {REFRESH_TODOS} from '../../constants/event'
-import {TITLE_MAX_LENGTH} from '../../constants/index'
-const {models, event} = getApp()
+import { regeneratorRuntime } from '../../lib/index'
+import { REFRESH_TODOS } from '../../constants/event'
+import { TITLE_MAX_LENGTH } from '../../constants/index'
+import debounce from 'debounce'
+import throttle from 'throttleit'
+import { showToast } from '../../utils/wx'
+const { models, event } = getApp()
 
 Page({
   data: {
@@ -9,45 +12,46 @@ Page({
     title: '',
     maxLength: TITLE_MAX_LENGTH + 1
   },
-  onLoad () {
+  onLoad() {
     event.on(REFRESH_TODOS, this.getTodos)
     this.getTodos()
   },
-  onInput ({detail}) {
+  async onPullDownRefresh() {
+    await this.getTodos()
+    wx.stopPullDownRefresh()
+  },
+  // 输入事件
+  onInput: debounce(function({ detail }) {
     let title = detail.value
     if (title.length > TITLE_MAX_LENGTH) {
-        wx.showToast({
-          title: `标题长度不能超过${TITLE_MAX_LENGTH}`,
-          icon: 'none'
-        })
-        title = title.substring(0, TITLE_MAX_LENGTH)
+      showToast(`标题长度不能超过${TITLE_MAX_LENGTH}`)
+      title = title.substring(0, TITLE_MAX_LENGTH)
     }
-    this.setData({title})
-  },
-  async addTodo () {
-    const {title} = this.data
-    if (!title.replace(/\s/g, '')) {
-      return wx.showToast({
-        title: '标题不能为空',
-        icon: 'none'
-      })
+    this.setData({ title })
+  }, 300),
+  // 添加任务
+  addTodo: throttle(async function() {
+    const { title } = this.data
+    if (!/[\S]+/g.test(title)) {
+      return showToast('标题不能为空')
     }
     try {
       wx.showNavigationBarLoading()
-      await models.todo.addTodo({title})
-      this.setData({title: ''})
+      await models.todo.addTodo({ title })
+      this.setData({ title: '' })
       this.getTodos()
     } catch (error) {
       console.error(error)
     } finally {
       wx.hideNavigationBarLoading()
     }
-  },
-  async getTodos () {
+  }, 1000),
+  // 获取任务
+  async getTodos() {
     try {
       wx.showNavigationBarLoading()
-      const {data} = await models.todo.getTodos()
-      this.setData({todos: data})
+      const { data } = await models.todo.getTodos()
+      this.setData({ todos: data })
     } catch (error) {
       console.error(error)
     } finally {
